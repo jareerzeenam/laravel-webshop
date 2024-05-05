db95fb8078674b581e19fcb6a2d4b78a202eb970
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-05-05 03:21:29 +0100

    Stripe customer shipping collection feature added

--- app/Actions/WebShop/CreateStripeCheckoutSession.php
+++ app/Actions/WebShop/CreateStripeCheckoutSession.php
@@ -14,7 +14,15 @@ class CreateStripeCheckoutSession
         return $cart->user
             ->allowPromotionCodes()
             ->checkout(
-            $this->formatCartItems($cart->items)
+            $this->formatCartItems($cart->items),
+                [
+                    'customer_update' => [
+                        'shipping'=>'auto',
+                    ],
+                    'shipping_address_collection' => [
+                        'allowed_countries' => ['GB','US','CA','AU'],
+                    ],
+                ]
         );
     }
 
