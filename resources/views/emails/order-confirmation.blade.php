063c654c3d86c7817ee095f780fbd0836520b9c9
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-05-24 20:47:27 +0100

    Order Confirmation Email Markdown Added

--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -5,80 +5,25 @@ Hi {{ $order->user->name }},
 
 Thank you for your order. We have received your order and we are currently processing it.
 
-Your order number is <b>(#{{ $order->id }})</b>.
+Your order number is **(#{{ $order->id }})**.
 
-<table style="width: 100%;">
-    <thead>
-        <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Tax</th>
-            <th>Total</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($order->items as $item)
-            <tr>
-                <td>{{ $item->name }} <br> {{$item->description}}</td>
-                <td>{{ $item->price }}</td>
-                <td>{{ $item->quantity }}</td>
-                <td>{{ $item->amount_tax }}</td>
-                <td>{{ $item->amount_total }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-    <tfoot>
-    @if($order->amount_shipping->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Shipping Fee
-            </td>
-            <td>
-                {{ $order->amount_shipping }}
-            </td>
-        </tr>
+@component('mail::table')
+    | Item          | Price         | Quantity  | Tax       |  Total     |
+    | ------------- |:-------------:| ---------:| --------: | ----------:|
+    @foreach($order->items as $item)
+    | **{{ $item->name }}** <br> {{ $item->description }} | {{ $item->price }} | {{ $item->quantity }} | {{ $item->amount_tax }} | {{ $order->amount_total }}|
+    @endforeach
+    @if(!$order->amount_shipping->isPositive())
+    ||||**Shipping**|{{ $order->amount_shipping }}|
     @endif
     @if($order->amount_discount->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Discount
-            </td>
-            <td>
-                {{ $order->amount_discount }}
-            </td>
-        </tr>
+    | | | | **Discount** | {{ $order->amount_discount }} |
     @endif
     @if($order->amount_tax->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Tax
-            </td>
-            <td>
-                {{ $order->amount_tax }}
-            </td>
-        </tr>
+    | | | | **Tax** | {{ $order->amount_tax }} |
     @endif
-    @if($order->amount_subtotal->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Subtotal
-            </td>
-            <td>
-                {{ $order->amount_subtotal }}
-            </td>
-        </tr>
-    @endif
-    @if($order->amount_total->isPositive())
-        <tr>
-            <td colspan="4" style="text-align: right;">
-                Total
-            </td>
-            <td style="text-align: right;">
-                {{ $order->amount_total }}
-            </td>
-        </tr>
-    @endif
-    </tfoot>
-</table>
+    | | | | **Subtotal** | {{ $order->amount_subtotal }} |
+    | | | | **Total** | **{{ $order->amount_total }}** |
+@endcomponent
+
 @endcomponent
