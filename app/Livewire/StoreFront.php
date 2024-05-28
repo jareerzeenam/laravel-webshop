f9a64d8cdcc1d1a0cf41311ecf9a5507f31ca5cb
Jareer Zeenam <jareerzeenam.28@gmail.com>
2024-05-28 02:10:07 +0100

    Search Products and pagination added

--- app/Livewire/StoreFront.php
+++ app/Livewire/StoreFront.php
@@ -4,15 +4,22 @@ namespace App\Livewire;
 
 use App\Models\Product;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class StoreFront extends Component
 {
+    use WithPagination;
 
+    #[Url]
+    public $keywords;
     #[Computed]
     public function products()
     {
-        return Product::query()->get();
+        return Product::query()
+            ->when($this->keywords, fn($query) => $query->where('name', 'like', "%{$this->keywords}%"))
+            ->paginate(10);
     }
 
     public function render()
--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,11 +1,24 @@
-<div class="grid grid-cols-4 gap-4 mt-12">
-    @foreach ($this->products as $product)
-        <x-panel>
-            <a wire:navigate href="{{ route('product.show', $product) }}" class="absolute inset-0 w-full h-full"></a>
-            <img src="{{ $product->image->path }}" alt="Product Image" class="w-full h-48 object-cover">
-            <h2 class="font-medium text-lg">{{ $product->name }}</h2>
-            <span class="text-gray-600 text-sm">{{ $product->price }}</span>
-            <p>{{ $product->description }}</p>
-        </x-panel>
-    @endforeach
+<div class="mt-12">
+    <div class="flex justify-between">
+        <h1 class="text-xl font-medium">Our Products</h1>
+        <div>
+            <x-input wire:model.live.debounce="keywords" type="search" placeholder="Enter Keywords" />
+        </div>
+    </div>
+    <div class="grid grid-cols-4 gap-4 mt-12">
+        @foreach ($this->products as $product)
+            <x-panel class="relative">
+                <a wire:navigate href="{{ route('product.show', $product) }}" class="absolute inset-0 w-full h-full"></a>
+                <img src="{{ $product->image->path }}" alt="Product Image" class="w-full h-48 object-cover">
+                <h2 class="font-medium text-lg">{{ $product->name }}</h2>
+                <span class="text-gray-600 text-sm">{{ $product->price }}</span>
+                <p>{{ $product->description }}</p>
+            </x-panel>
+        @endforeach
+    </div>
+
+    <div class="mt-6">
+        {{ $this->products->links() }}
+    </div>
+
 </div>
